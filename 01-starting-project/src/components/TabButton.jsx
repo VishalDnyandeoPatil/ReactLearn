cbe8cb72c645926cb80a4c0c4fe95397a9290fb1
unknown <vp1610858@gmail.com>
2025-07-16 22:13:36 +0530

    Start again react learning

--- 01-starting-project/src/components/TabButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function TabButton({ children, onSelect, isSelected}) {
-  console.log("Tab button component executed")
-
-    return (
-    <li>
-      <button className= {isSelected ? "active" : ""} onClick={onSelect}>{children}</button>
-    </li>
-  );
-}
