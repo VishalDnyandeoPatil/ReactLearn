edd37c22ec83d96f54dfef240a3d28ef7eeaab10
VishalDnyandeoPatil <vp1610858@gmail.com>
2025-07-27 13:30:45 +0530

    Update data.js file and learn dynamic use state

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import  Header  from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
-  const [selectedTopic, setSelectedTopic]= useState('Please click a button')
+  const [selectedTopic, setSelectedTopic]= useState('components')
   
   function handelSelect(selectedButton) {
         setSelectedTopic(selectedButton)
@@ -35,7 +35,15 @@ function App() {
             <TabButton  onSelect={()=>handelSelect('props')}>Props</TabButton>
             <TabButton  onSelect={()=>handelSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
