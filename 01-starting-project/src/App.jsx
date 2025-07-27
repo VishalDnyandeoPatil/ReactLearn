b01f525866799c1cc06ee696ed16fa0250f4d90e
VishalDnyandeoPatil <vp1610858@gmail.com>
2025-07-27 15:31:10 +0530

    Learn conditional base change state

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 import { CORE_CONCEPTS, EXAMPLES } from "./data";
-import  Header  from "./components/Header/Header";
+import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
-  const [selectedTopic, setSelectedTopic]= useState('components')
-  
-  function handelSelect(selectedButton) {
-        setSelectedTopic(selectedButton)
-    }
+  const [selectedTopic, setSelectedTopic] = useState();
 
-    console.log("App component executing")
+  function handelSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  console.log("App component executing");
+
+  let tabContent = <p>Please select a topic.</p>
+
+  if(selectedTopic){
+    tabContent =  (<div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>)
+  }
 
   return (
     <div>
@@ -21,29 +33,23 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <section id = "examples">
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton  onSelect={()=>handelSelect('components')}>Components</TabButton>
-            <TabButton  onSelect={()=>handelSelect('jsx')}>Jsx</TabButton>
-            <TabButton  onSelect={()=>handelSelect('props')}>Props</TabButton>
-            <TabButton  onSelect={()=>handelSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => handelSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handelSelect("jsx")}>Jsx</TabButton>
+            <TabButton onSelect={() => handelSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handelSelect("state")}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
