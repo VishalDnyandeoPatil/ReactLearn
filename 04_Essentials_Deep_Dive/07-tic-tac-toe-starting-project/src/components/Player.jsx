1a6d57f911ab68ef84b9e054b94ff98cfc43e16c
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-07-31 15:11:13 +0530

    learn conditional content and a suboptimal way of updating state

--- 04_Essentials_Deep_Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04_Essentials_Deep_Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,21 +4,21 @@ export default function Player({name, symbol}){
     const [isEditing, setIsEditing]= useState(false);
 
     function handelEditClick(){
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
     let playerName = <span className="player-name">{name}</span>;
+    // let btnCaption = "Edit"
 
     if(isEditing){
-        playerName= <input type="text" required />
+        playerName= <input type="text" required value={name} />;
+        // btnCaption="Save";
     }
-
-   
 
     return  (<li>
     <span className="player">
         {playerName}
     <span className="player-symbol">{symbol}</span>
     </span>
-    <button onClick={handelEditClick}> Edit</button>
+    <button onClick={handelEditClick}>{isEditing? 'Save': 'Edit'}</button>
   </li>)
 }
