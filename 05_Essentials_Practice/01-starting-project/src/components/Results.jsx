cd54b73e8a5e985fde2f87c3eb33d1d62a9b66cb
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-02 17:21:56 +0530

    Computing values and properly handling number values

--- 05_Essentials_Practice/01-starting-project/src/components/Results.jsx
+++ 05_Essentials_Practice/01-starting-project/src/components/Results.jsx
@@ -1,4 +1,7 @@
+import {calculateInvestmentResults} from '../util/investment'
+
 export default function Results ({input}){
-    console.log(input)
+    const resultData = calculateInvestmentResults(input)
+    
     return <p> Results...</p>
 }
