7ffc77093d0943e8329f0fd79ee7ce48fe4df18c
VishalDnyandeoPatil <vp1610858@gmail.com>
2025-08-04 21:14:11 +0530

    Creating reusable components and components combinations

--- 06_Styling_React_Component/01-starting-project/src/components/Header.jsx
+++ 06_Styling_React_Component/01-starting-project/src/components/Header.jsx
@@ -1,22 +1,22 @@
-import {styled} from 'styled-components'
+import { styled } from "styled-components";
 import logo from "../assets/logo.png";
 // import classes from "./Header.module.css";
 
 const StyledHeader = styled.header`
-display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 2rem;
-    margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
 
-    & img {
+  & img {
     object-fit: contain;
     margin-bottom: 2rem;
     width: 11rem;
     height: 11rem;
   }
-  
+
   & h1 {
     font-size: 1.5rem;
     font-weight: 600;
@@ -24,34 +24,31 @@ display: flex;
     text-align: center;
     text-transform: uppercase;
     color: #9a3412;
-    font-family: 'Pacifico', cursive;
+    font-family: "Pacifico", cursive;
     margin: 0;
   }
-  
+
   & p {
     text-align: center;
     color: #a39191;
     margin: 0;
   }
-  
+
   @media (min-width: 768px) {
-    
-      margin-bottom: 4rem;
-  
+    margin-bottom: 4rem;
+
     & h1 {
       font-size: 2.25rem;
     }
   }
-`
+`;
 
 export default function Header() {
   return (
     <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>
-        A community of artists and art-lovers.
-      </p>
+      <p>A community of artists and art-lovers.</p>
     </StyledHeader>
   );
 }
