58fc9fdb4063166354381a59d457e7fcde0b0a56
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-06 16:51:21 +0530

    Adding challenges in project

--- 08_Refs_Portals/01-starting-project/src/components/Player.jsx
+++ 08_Refs_Portals/01-starting-project/src/components/Player.jsx
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 export default function Player() {
   const playerName = useRef();
+
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handelClick() {
@@ -11,7 +12,7 @@ export default function Player() {
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome { enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input
           ref={playerName}
