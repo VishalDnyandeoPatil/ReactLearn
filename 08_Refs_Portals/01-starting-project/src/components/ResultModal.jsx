042bc3ee874b0ccf14bcb9b1a47e05d57f387cd7
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-07 14:55:59 +0530

    Use portals

--- 08_Refs_Portals/01-starting-project/src/components/ResultModal.jsx
+++ 08_Refs_Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom'
 
 export default function ResultModel({ ref, targetTime, remainingTime, onReset }) {
 
@@ -16,8 +17,8 @@ export default function ResultModel({ ref, targetTime, remainingTime, onReset })
     }
   });
 
-  return (
-    <dialog ref={dialog} className="result-modal">
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost&& <h2>Your Score: {score}</h2> }
       <p>
@@ -29,6 +30,7 @@ export default function ResultModel({ ref, targetTime, remainingTime, onReset })
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 }
