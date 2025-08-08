0b89f6855c0f49426b8c9fca7933ceb5f3f44f94
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-08 17:50:43 +0530

    Add clearing task logic and fixing minor bugs

--- 09_Project_Management_App/01-starting-project/src/App.jsx
+++ 09_Project_Management_App/01-starting-project/src/App.jsx
@@ -26,7 +26,16 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(
+          (task) => task.id !== id
+        ),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
@@ -75,7 +84,7 @@ function App() {
       return {
         ...prevState,
         selectedProjectId: undefined,
-        projects: projectsState.projects.filter(
+        projects: prevState.projects.filter(
           (project) => project.id !== prevState.selectedProjectId
         ),
       };
@@ -110,6 +119,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
