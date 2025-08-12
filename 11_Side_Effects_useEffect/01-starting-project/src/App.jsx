b100e878bf6b75ce2571a1b157a850285ea053b8
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-12 10:52:49 +0530

    Another use case for use effect

--- 11_Side_Effects_useEffect/01-starting-project/src/App.jsx
+++ 11_Side_Effects_useEffect/01-starting-project/src/App.jsx
@@ -15,8 +15,8 @@ const storePlaces = storeIds.map((id) =>
 
 function App() {
  
-  const modal = useRef();
   const selectedPlace = useRef();
+  const[modalIsOpen, setModalIsOpen] = useState(false)
   const [availablePlaces, setAvilablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storePlaces);
 
@@ -32,12 +32,12 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -59,7 +59,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storeIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem(
@@ -70,7 +70,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
