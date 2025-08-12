81f61d54b42dffc0869a67e6a4fed43338459060
VishalPatilKryzen <vishal.patil@kryzen.com>
2025-08-12 12:23:53 +0530

    useEffects clean up function

--- 11_Side_Effects_useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 11_Side_Effects_useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  console.log("Timmer set")
-  
-  setTimeout(()=>{
+  useEffect(()=>{
+    console.log("Timer set")
+  const timer = setTimeout(()=>{
     onConfirm();
   },3000);
+  return () =>{
+    console.log("Cleaning up timer")
+    clearTimeout(timer)
+  }
+  },[])
+  
 
   return (
     <div id="delete-confirmation">
