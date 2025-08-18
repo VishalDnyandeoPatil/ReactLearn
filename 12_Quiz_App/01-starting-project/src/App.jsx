6c39cf3f211d4762a9855118895cf2f4668ed898
VishalDnyandeoPatil <vp1610858@gmail.com>
2025-08-18 22:28:29 +0530

    Add components in quiz project

--- 12_Quiz_App/01-starting-project/src/App.jsx
+++ 12_Quiz_App/01-starting-project/src/App.jsx
@@ -1,3 +1,13 @@
-function App() {}
+import Header from "../src/components/Header.jsx";
+import Quiz from "../src/components/Quiz.jsx"
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Quiz></Quiz>
+    </>
+  );
+}
 
 export default App;
